54a1d5290d2783f763a68a15cc393db47a8432ce
Erich Elste <erichelste@yahoo.com>
2022-06-13 03:00:33 -0400

    idex and generate html

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+
+
+
+const staff= [];
+
+function spawnManager () {
+    inquirer
+        .prompt([
+            {type: 'input', message: "Enter the manager's name", name: 'name'},
+            {type: 'input', message: "Enter the manager's employee ID", name: 'employee'},
+            {type: 'input', message: "Enter the manager's address", name: 'email'},
+            {type: 'input', message: "Enter the manager's office number", name: 'office'},
+        ])
+        .then ((response) => {
+            let manager = new Manager ( response.name, response.employee, response.email, response.office)
+            staff.push(manager)
+            spawnEmployee();
+        })
+}
+
+function spawnEmployee() {
+    inquirer
+        .prompt([
+            {type: 'list', message: "Is this new employee an Engineer or Intern?", name: 'role', choices: ['Engineer','Intern']},
+            {type: 'input', message: "Enter the employee's employee name", name: 'name'},
+            {type: 'input', message: "Enter the employee's employee ID", name: 'employee'},
+            {type: 'input', message: "Enter the employee's email address", name: 'email'},
+            {type: 'input', message: "Enter the employee's github username", when: (list) => list.role == "Engineer", name: "github"},
+            {type: 'input', message: "Enter the employee's school", when: (list) => list.role == "Intern", name: "school" },
+            {type: 'input', message: "Would you like to add another employee", name: "add"},
+        ])
+        .then((response) => {
+            if(response.role == "Intern") {
+                
+                let intern = new Intern ( response.name, response.employee, response.email, response.school)
+                staff.push(intern)
+            }
+            else if(response.role == "Engineer") {
+                
+                let engineer = new Enginner ( response.name, response.employee, response.email, response.github)
+                staff.push(engineer)
+            }
+            if(response.add == true)
+            {spawnEmployee();}
+            else
+            {spawnHTML}
+        })
+}
---
+++ src/generateHTML.js
@@ -0,0 +1,99 @@
+const makeManager = function (manager) {
+    return `<div class="card">
+    <div class="card-top">
+        <h1>${manager.name}</h1>
+        <h2>
+            ${manager.getRole()}
+        </h2>
+    </div>
+    <div class="card-bottom">
+        <p>ID: ${manager.id}</p>
+        <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+        <p>${manager.officeNumber}</p>
+    </div>
+</div>`
+}
+
+const makeIntern = function (intern) {
+    return `<div class="card">
+    <div class="card-top">
+        <h1>${intern.name}</h1>
+        <h2>
+            ${intern.getRole()}
+        </h2>
+    </div>
+    <div class="card-bottom">
+        <p>ID: ${intern.id}</p>
+        <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+        <p>${intern.school}</p>
+    </div>
+</div>`
+}
+
+const makeEngineer = function (engineer){
+    return `<div class="card">
+    <div class="card-top">
+        <h1>${engineer.name}</h1>
+        <h2>
+            ${engineer.getRole()}
+        </h2>
+    </div>
+    <div class="card-bottom">
+        <p>ID: ${engineer.id}</p>
+        <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+        <p>Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+    </div>
+</div>`
+}
+
+
+function makeHTML(team){
+
+    const managerArray = [];
+    const internArray = [];
+    const engineerArray = [];
+
+    for(let i = 0; i < team.length; i++)
+    {
+        let employee = team[i];
+        let role = employee.getRole();
+
+        switch(role)
+        {
+            case "Manager":
+            managerArray.push(makeManager(employee))
+            break;
+
+            case "Intern":
+            internArray.push(makeIntern(employee))
+            break;
+
+            case "Engineer":
+            engineerArray.push(makeEngineer(employee))
+            break;
+        }
+    }
+
+return `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href = "./FinalHTML/reset.css" rel="stylesheet">
+    <link href = "./FinalHTML/style.css" rel="stylesheet">
+    <title>Employees</title>
+</head>
+<header>
+    <h1>My Team</h1>
+</header>
+<body>
+    <section>
+    ${managerArray.join('')}
+    ${internArray.join('')}
+    ${engineerArray.join('')}
+    </section>
+</body>
+</html>`
+}
+
+module.exports = makeHTML;
